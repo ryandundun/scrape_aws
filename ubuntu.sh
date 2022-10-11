96a59551c084f6e1fdeb13532ae2958ad32c240b
Ryan Dundun <rdundun3@gmail.com>
2022-10-11 00:54:41 -0500

    Update

--- ubuntu.sh
+++ ubuntu.sh
@@ -10,11 +10,14 @@
 # git clone https://github.com/ryandundun/scrape_aws.git
 # cd scrape_aws
 
+#to execute shell script
+# chmod +x ./ubuntu.sh
+# ./ubuntu.sh
+
 #to install all requirements
 pip3 install -r requirements.txt
 
 #extra commmand for playwright
-playwright install
-python -m playwright install
+python3 -m playwright install
 python3 -m playwright install-deps
 
